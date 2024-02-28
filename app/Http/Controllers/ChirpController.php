0829ec7ed409f71e572ed32f9f9dcbcd47b0c01c
IrinaF <ira523523523@gmail.com>
2024-02-28 17:02:04 +0300

    show and edit posts

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -15,7 +15,9 @@ class ChirpController extends Controller
      */
     public function index(): Response
     {
-        return Inertia::render('Chirps/Index', []);
+        return Inertia::render('Chirps/Index', [
+            'chirps' => Chirp::with('user:id,name')->latest()->get(),
+        ]);
     }
 
     /**
@@ -59,9 +61,18 @@ class ChirpController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Chirp $chirp)
+    public function update(Request $request, Chirp $chirp): RedirectResponse
     {
         //
+        $this->authorize('update', $chirp);
+
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $chirp->update($validated);
+
+        return redirect(route('chirps.index'));
     }
 
     /**
